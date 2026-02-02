5cf386dbb9610650e2afc332e49a2454c361ef73
Leo Girard <leo.girard@epitech.eu>
2026-02-02 14:21:37 +0100

    implementation gravity but need fixes with movements

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ in terminal type 'make hello_world run delete'
 
 # TODO
 
-Make gravity like inertie or more realistic 'not instant'
+Fix gravity
--- hello_world.cpp
+++ hello_world.cpp
@@ -5,58 +5,73 @@
 #include <SFML/Window/Keyboard.hpp>
 #include <iostream>
 
+void UserInput(std::optional<sf::Event> &event, sf::Sprite &sprite, sf::Vector2f &velocity) {
 
+  float moveSpeed = 5.0f;
+  float jumpSpeed = 30.0f;
+    std::cout << sprite.getPosition().x << "" << sprite.getPosition().y
+  << std::endl;
 
-void UserInput(std::optional<sf::Event>& event, sf::Sprite& sprite) {
-    if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
-        if (keyPressed->scancode == sf::Keyboard::Scancode::D) {
-            sprite.move(sf::Vector2f(5.0f, 0.0f));
-        }
-        if (keyPressed->scancode == sf::Keyboard::Scancode::A) {
-            sprite.move(sf::Vector2f(-5.0f, 0.0f));
-        }
-        if (keyPressed->scancode == sf::Keyboard::Scancode::W) {
-            sprite.move(sf::Vector2f(0.0f, -5.0f));
-        }
-        if (keyPressed->scancode == sf::Keyboard::Scancode::S) {
-            sprite.move(sf::Vector2f(0.0f, 5.0f));
-        }
+  if (const auto *keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+    if (keyPressed->scancode == sf::Keyboard::Scancode::D) {
+      velocity.x = moveSpeed;
     }
+    if (keyPressed->scancode == sf::Keyboard::Scancode::A) {
+      velocity.x = -moveSpeed;
+    }
+    if (keyPressed->scancode == sf::Keyboard::Scancode::W) {
+      velocity.y = -jumpSpeed;
+    }
+  }
+
+  if (const auto *keyReleased = event->getIf<sf::Event::KeyReleased>()) {
+    if (keyReleased->scancode == sf::Keyboard::Scancode::D ||
+        keyReleased->scancode == sf::Keyboard::Scancode::A) {
+      velocity.x = 0;
+    }
+  }
 }
 
+void UserGravity(sf::Sprite &sprite, sf::Vector2f &velocity) {
+  const float gravity = 5;
+  const float Ground = 250;
 
-void UserGravity(sf::Sprite& sprite) {
-    sf::Vector2f position = sprite.getPosition();
-    if (position.y != 250) {
-        sprite.move(sf::Vector2(0.0f , 0.5f));
-    }
+  if (sprite.getPosition().y < Ground) {
+    velocity.y += gravity;
+  } else {
+    velocity.y = 0;
+  }
+  sprite.move(velocity);
 }
-
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Super video game.com");
-    sf::Texture texture;
-    if (!texture.loadFromFile("images/mushroom2.jpg")) {
-        std::cout << "image not working" << std::endl;
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "Super videogame.com");
+  sf::Texture texture;
+  if (!texture.loadFromFile("images/mushroom2.jpg")) {
+    std::cout << "image not working" << std::endl;
+  }
+  sf::Sprite sprite(texture);
+  sprite.setScale(sf::Vector2f(0.1, 0.1));
+  sprite.setPosition(sf::Vector2f(120, 120));
+  sf::Vector2f velocity(0, 0);
+
+  while (window.isOpen()) {
+    while (auto event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>())
+        window.close();
+
+      UserInput(event, sprite, velocity);
     }
-    sf::Sprite sprite(texture);
-    sprite.setScale(sf::Vector2f(0.1, 0.1));
-    sprite.setPosition(sf::Vector2f(120, 120));
-    
-    while (window.isOpen()) {
-        while (auto event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-            
-            UserInput(event, sprite);
-        }
-        
-        UserGravity(sprite);
-        window.clear();
-        window.draw(sprite);
-        window.display();
-    }
-    
-    return 0;
+
+    UserGravity(sprite, velocity);
+    sprite.move(velocity);
+    window.clear();
+    window.draw(sprite);
+    window.display();
+  }
+
+  return 0;
 }
-//make the gravity which moves the sprite to ground
+
+// find a way to update user gravity automatically, bc now it is tagged
+// undeclared
